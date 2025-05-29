caf427b81dbde8d9a14a13de7f5c8ffd20e5e967
ValentinaCortes09 <balentina.09.1991@gmail.com>
2025-05-29 20:51:35 +0000

    nuevo

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getFavorites, removeFromFavorites } from '../utils/favorites';
 
+
 const Favorites = () => {
   const [favorites, setFavorites] = useState([]);
 
